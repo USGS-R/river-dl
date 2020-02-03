22335b44bec44d0b36d4aceabe85b2005cde5de7
jsadler2 <jsadler@usgs.gov>
2020-02-03 17:08:35 -0600

    simple lstm for streamflow working

--- simple_streamflow_lstm.py
+++ simple_streamflow_lstm.py
@@ -1,8 +1,9 @@
+import random
+from random import seed
+import numpy as np
 import tensorflow as tf
 import pandas as pd
 from sklearn import preprocessing
-from random import seed
-import random
 
 
 def select_data(df):
@@ -23,7 +24,7 @@ def get_df_for_rand_seg(df):
 def scale(df):
     std = df.std()
     mean = df.mean()
-    scaled = preprocessing.scale()
+    scaled = preprocessing.scale(df)
     return scaled, std, mean
 
 df = pd.read_feather('data/sntemp_input_output_subset.feather')
@@ -32,7 +33,7 @@ df_seg = get_df_for_rand_seg(df)
 
 # separate the testing from the training 80/20
 nrows = df_seg.shape[0]
-sep_idx = nrows * 0.8 
+sep_idx = int(nrows * 0.8)
 df_trn = df_seg.iloc[:sep_idx, :]
 df_tst = df_seg.iloc[sep_idx:, :]
 
@@ -41,9 +42,19 @@ predictors_trn, target_trn = select_data(df_trn)
 predictors_tst, target_tst = select_data(df_tst)
 
 x_trn, x_trn_std, x_trn_mean = scale(predictors_trn)
+# add a dimension for the timesteps (just using 1)
+x_trn = np.expand_dims(x_trn, axis=1)
 y_trn, y_trn_std, y_trn_mean = scale(target_trn)
 
-x_tst, x_tst_std, x_tst_mean = scale(predictors_tst)
-y_tst, y_tst_std, y_tst_mean = scale(target_tst)
+trn = tf.data.Dataset.from_tensor_slices((x_trn, y_trn))
+trn = trn.batch(365).shuffle(365)
 
+x_tst = scale(predictors_tst)[0]
+y_tst = scale(target_tst)[0]
+tst = tf.data.Dataset.from_tensor_slices((x_tst, y_tst))
 
+simple_lstm_model = tf.keras.models.Sequential([tf.keras.layers.LSTM(10),
+                                                tf.keras.layers.Dense(1)])
+
+simple_lstm_model.compile(optimizer='adam', loss='mae')
+simple_lstm_model.fit(trn, epochs=10)

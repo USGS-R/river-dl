7abaf67cd895fd7f53cc27ecb1c1f2d81e56f9dc
jsadler2 <jsadler@usgs.gov>
2020-03-16 15:30:18 -0500

    animation works

--- plots/animation.py
+++ plots/animation.py
@@ -4,17 +4,10 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
-def animate(date, gdf, ax):
-    ax = gdf.plot(linewidth=gdf[date]**0.25, ax=ax)
-    ax.text(0, 0, date, transform=ax.transAxes)
-    return ax
-
-
 dfq = pd.read_csv('../data/obs_flow_full.csv')
 gdf = gpd.read_file('../data/gis/Segments_subset.shp')
 
 dfq['date'] = pd.to_datetime(dfq['date'])
-# dfq.set_index('seg_id_nat', inplace=True)
 dfqp = dfq.pivot(values='discharge_cms', index='seg_id_nat', columns='date')
 dfqp.fillna(0, inplace=True)
 dfqp.columns = dfqp.columns.astype(str)
@@ -24,10 +17,15 @@ gdf.set_index('seg_id_nat', inplace=True)
 gdf_comb = gdf.join(dfqp, how='outer')
 
 fig, ax = plt.subplots()
-ax = gdf_comb.plot(linewidth=0.5, color='gray', ax=ax)
+
+
+def animate(date):
+    ax.clear()
+    my_ax = gdf_comb.plot(linewidth=0.5, color='gray', ax=ax)
+    my_ax = gdf_comb.plot(linewidth=gdf_comb[date]**0.25, ax=my_ax)
+    my_ax.text(0, 0, date, transform=ax.transAxes)
 
 ani_dates = dates[500:525]
-anim = FuncAnimation(fig, animate, frames=ani_dates, fargs=(gdf_comb, ax))
+anim = FuncAnimation(fig, animate, frames=ani_dates)
 anim.save('flow.gif', writer='imagemagick')
-
 

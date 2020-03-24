c51400de6bef41c25d5502560898a2903bc13da0
jsadler2 <jsadler@usgs.gov>
2020-03-24 16:41:04 -0500

    removed plots from repo

--- plots/animation.py
+++ plots/animation.py
@@ -7,7 +7,9 @@ from matplotlib.animation import FuncAnimation
 dfq = pd.read_csv('../data/obs_flow_full.csv')
 gdf = gpd.read_file('../data/gis/Segments_subset.shp')
 
+start_date = '1980-01-01'
 dfq['date'] = pd.to_datetime(dfq['date'])
+dfq = dfq[dfq['date'] > start_date]
 dfqp = dfq.pivot(values='discharge_cms', index='seg_id_nat', columns='date')
 dfqp.fillna(0, inplace=True)
 dfqp.columns = dfqp.columns.astype(str)
@@ -21,11 +23,10 @@ fig, ax = plt.subplots()
 
 def animate(date):
     ax.clear()
-    my_ax = gdf_comb.plot(linewidth=0.5, color='gray', ax=ax)
+    my_ax = gdf_comb.plot(linewidth=0.5, color='lightgray', ax=ax)
     my_ax = gdf_comb.plot(linewidth=gdf_comb[date]**0.25, ax=my_ax)
     my_ax.text(0, 0, date, transform=ax.transAxes)
 
-ani_dates = dates[500:525]
-anim = FuncAnimation(fig, animate, frames=ani_dates)
+anim = FuncAnimation(fig, animate, frames=dates[:100])
 anim.save('flow.gif', writer='imagemagick')
 

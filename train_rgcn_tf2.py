951523d7f00cc32edba6f71822571731ff55211b
jsadler2 <jsadler@usgs.gov>
2020-04-13 16:50:21 -0500

    [#21] training with weights argument, tst date variable

--- train_rgcn_tf2.py
+++ train_rgcn_tf2.py
@@ -1,8 +1,9 @@
 import argparse
+import numpy as np
 import datetime
 import tensorflow as tf
 from RGCN_tf2 import RGCNModel, rmse_masked
-from data_utils import read_process_data, process_adj_matrix
+from data_utils import read_process_data, read_exclude_segs_file
 
 start_time = datetime.datetime.now()
 
@@ -25,6 +26,10 @@ parser.add_argument("-f", "--finetune_vars", help='whether to finetune on\
                     temp, flow, or both', choices=['temp', 'flow', 'both'])
 parser.add_argument("-p", "--pretrain_vars", help='whether to pretrain on\
                     temp, flow, or both', choices=['temp', 'flow', 'both'])
+parser.add_argument('-s', '--start_tst_date', help='date when test period \
+                    starts', default='2004-09-30')
+parser.add_argument('-y', '--y_tst_year', help='number of years in test set',
+                    default=12, type=int)
 parser.add_argument('-i', '--input_data_dir', help='directory where input data\
                     are located')
 parser.add_argument("-d", "--dist_matrix", help='which type of distance matrix\
@@ -32,6 +37,8 @@ parser.add_argument("-d", "--dist_matrix", help='which type of distance matrix\
                     default='upstream')
 parser.add_argument("-t", "--tag", help='tag to append to end of file',
                     default='')
+parser.add_argument("-x", "--exclude_segs_file", help='yml file that contains\
+                    segments to exclude', default=None)
 args = parser.parse_args()
 
 in_data_dir = args.input_data_dir
@@ -40,6 +47,10 @@ out_dir = args.outdir
 pt_vars = args.pretrain_vars
 ft_vars = args.finetune_vars
 dist_mat = args.dist_matrix
+if args.exclued_segs_file:
+    exclude_segs = read_exclude_segs_file(args.exclude_segs_file)
+else:
+    exclude_segs = []
 tag = args.tag
 if tag != '':
     tag = f'_{tag}'
@@ -51,10 +62,14 @@ elif network == "subset":
     subset = True
 
 # set up model/read in data
-data = read_process_data(in_data_dir, subset=subset, trn_ratio=0.67,
-                         batch_offset=batch_offset,
-                         pretrain_out_vars=pt_vars, finetune_out_vars=ft_vars,
-                         dist_type=dist_mat)
+data = read_process_data(in_data_dir,
+                         subset=subset,
+                         pretrain_out_vars=pt_vars,
+                         finetune_out_vars=ft_vars,
+                         dist_type=dist_mat,
+                         test_start_date=args.y_tst_year,
+                         exclude_segs=exclude_segs)
+
 n_seg = data['dist_matrix'].shape[0]
 model = RGCNModel(hidden_size, 2, A=data['dist_matrix'])
 
@@ -66,7 +81,8 @@ csv_log_pre = tf.keras.callbacks.CSVLogger(f'{out_dir}pretrain_log_{network}'
                                            f'{tag}.csv')
 
 x_trn_pre = data['x_trn_pre']
-y_trn_pre = data['y_trn_pre']
+# combine with weights to pass to loss function
+y_trn_pre = np.hstack([data['y_trn_pre'], data['y_pre_wgts']])
 
 model.fit(x=x_trn_pre, y=y_trn_pre, epochs=epochs_pre, batch_size=n_seg,
           callbacks=[csv_log_pre])
@@ -88,7 +104,7 @@ csv_log_ft = tf.keras.callbacks.CSVLogger(f'{out_dir}finetune_log_{network}'
                                           f'{tag}.csv')
 
 x_trn_obs = data['x_trn']
-y_trn_obs = data['y_trn_obs']
+y_trn_obs = np.hstack(data['y_trn_obs'], data['y_obs_wgts'])
 
 model.fit(x=x_trn_obs, y=y_trn_obs, epochs=epochs_finetune, batch_size=n_seg,
           callbacks=[csv_log_ft])

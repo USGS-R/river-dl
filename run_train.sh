5f8671f91bb571e800e244ff85437eda26e73e1b
jsadler2 <jsadler@usgs.gov>
2020-04-16 19:46:44 -0500

    update run_predict, run_train with new fnames

--- run_train.sh
+++ run_train.sh
@@ -1,2 +1,2 @@
-python code/train_rgcn_tf2.py -i data/in/prepped/processed_data.npz\
-                              -o data/out/
+python code/train.py -i data/in/prepped/processed_data.npz\
+                     -o data/out/

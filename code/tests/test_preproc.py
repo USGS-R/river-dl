eb0dedfc40efffd4da50e37e86f29b31c1e1ab0c
jsadler2 <jsadler@usgs.gov>
2020-06-04 14:14:53 -0500

    restructure test preproc with class

--- code/tests/test_preproc.py
+++ code/tests/test_preproc.py
@@ -58,63 +58,74 @@ def test_read_exclude():
                    {'seg_id_nats': [2012], 'end_date': '2005-09-15'}]
 
 
+class PreppedData:
+    def __init__(self, ft_vars=['seg_tave_water', 'seg_outflow'],
+                 pt_vars=['seg_tave_water', 'seg_outflow'],
+                 x_cols=['seg_tave_air', 'seg_rain'],
+                 test_st='2005-09-15', n_test_yr=1, exclude_file=None):
+        self.x_data_file = 'test_data/x_data.npz'
+        self.x_data = preproc_utils.prep_x(sntemp, x_cols,
+                                           test_start_date=test_st,
+                                           n_test_yr=n_test_yr,
+                                           out_file=self.x_data_file)
+        self.y_data = preproc_utils.prep_y(obs_temp, obs_flow, sntemp,
+                                           self.x_data_file,
+                                           pt_vars, ft_vars,
+                                           exclude_file=exclude_file)
+
+        self.sample_x = postproc_utils.prepped_array_to_df(
+            self.x_data['x_trn'], self.x_data['dates_trn'],
+            self.x_data['ids_trn'], self.x_data[ 'x_cols']).set_index(
+            ['seg_id_nat', 'date']).to_xarray()
+
+        self.sample_y = postproc_utils.prepped_array_to_df(
+            self.y_data['y_obs_trn'],
+            self.x_data['dates_trn'],
+            self.x_data['ids_trn'],
+            self.y_data['y_vars_ft']).set_index(
+            ['seg_id_nat', 'date']).to_xarray()
+
+        # read in unprocessed observations/inputs
+        self.obs_y_flow = pd.read_csv(obs_flow, parse_dates=['date']).set_index(
+            ['seg_id_nat', 'date']).to_xarray()
+        self.obs_y_temp = pd.read_csv(obs_temp, parse_dates=['date']).set_index(
+            ['seg_id_nat', 'date']).to_xarray()
+        self.sntemp_x = xr.open_zarr(sntemp)
+
+
 def test_prep():
     """
     testing whether I can reconstruct the original data after processing
     :return:
     """
-    x_data_file = 'test_data/x_data.npz'
-    x_data = preproc_utils.prep_x(sntemp, ['seg_tave_air', 'seg_rain'],
-                                  test_start_date='2005-09-15', n_test_yr=1,
-                                  out_file=x_data_file)
-    ft_vars = ['seg_tave_water', 'seg_outflow']
-    pt_vars = ft_vars
-    y_data = preproc_utils.prep_y(obs_temp, obs_flow, sntemp, x_data_file,
-                                  pt_vars, ft_vars, "test_data/exclude.yml")
-
-    sample_x = postproc_utils.prepped_array_to_df(x_data['x_trn'], x_data['dates_trn'],
-                                                  x_data['ids_trn'],
-                                                  x_data['x_cols']).set_index(
-        ['seg_id_nat', 'date']).to_xarray()
-    sample_y = postproc_utils.prepped_array_to_df(y_data['y_obs_trn'],
-                                                  x_data['dates_trn'],
-                                                  x_data['ids_trn'],
-                                                  y_data['y_vars_ft']).set_index(
-        ['seg_id_nat', 'date']).to_xarray()
-
-    # read in unprocessed observations/inputs
-    obs_y_flow = pd.read_csv(obs_flow, parse_dates=['date']).set_index(
-        ['seg_id_nat', 'date']).to_xarray()
-    obs_y_temp = pd.read_csv(obs_temp, parse_dates=['date']).set_index(
-        ['seg_id_nat', 'date']).to_xarray()
-    sntemp_x = xr.open_zarr(sntemp)
+    prepped = PreppedData()
 
     # make sure they are the same
     # air temp
-    sntemp_air_t = sntemp_x['seg_tave_air'].loc[:, sample_x.date].values
-    processed_air_t = sample_x['seg_tave_air'].loc[:, sample_x.date].values
-    processed_air_t = processed_air_t * x_data['x_std'][0] + x_data['x_mean'][0]
+    sntemp_air_t = prepped.sntemp_x['seg_tave_air'].loc[:, prepped.sample_x.date].values
+    processed_air_t = prepped.sample_x['seg_tave_air'].loc[:, prepped.sample_x.date].values
+    processed_air_t = processed_air_t * prepped.x_data['x_std'][0] + prepped.x_data['x_mean'][0]
     assert np.allclose(processed_air_t, sntemp_air_t)
 
     # rain
-    sntemp_r = sntemp_x['seg_rain'].loc[:, sample_x.date].values
-    processed = sample_x['seg_rain'].loc[:, sample_x.date].values
-    processed = processed * x_data['x_std'][1] + x_data['x_mean'][1]
+    sntemp_r = prepped.sntemp_x['seg_rain'].loc[:, prepped.sample_x.date].values
+    processed = prepped.sample_x['seg_rain'].loc[:, prepped.sample_x.date].values
+    processed = processed * prepped.x_data['x_std'][1] + prepped.x_data['x_mean'][1]
     assert np.allclose(processed, sntemp_r)
 
     # temp
-    obs = obs_y_temp['temp_c'].loc[:, sample_y.date].values
-    processed = sample_y['seg_tave_water'].loc[:, sample_y.date].values
-    processed = processed * y_data['y_obs_trn_std'][0] + \
-                y_data['y_obs_trn_mean'][0]
+    obs = prepped.obs_y_temp['temp_c'].loc[:, prepped.sample_y.date].values
+    processed = prepped.sample_y['seg_tave_water'].loc[:, prepped.sample_y.date].values
+    processed = processed * prepped.y_data['y_obs_trn_std'][0] + \
+                prepped.y_data['y_obs_trn_mean'][0]
     mask = ~(np.isnan(obs))
     assert np.allclose(processed[mask], obs[mask])
 
     # flow
-    obs = obs_y_flow['discharge_cms'].loc[:, sample_y.date].values
-    processed = sample_y['seg_outflow'].loc[:, sample_y.date].values
-    processed = processed * y_data['y_obs_trn_std'][1] + \
-                y_data['y_obs_trn_mean'][1]
+    obs = prepped.obs_y_flow['discharge_cms'].loc[:, prepped.sample_y.date].values
+    processed = prepped.sample_y['seg_outflow'].loc[:, prepped.sample_y.date].values
+    processed = processed * prepped.y_data['y_obs_trn_std'][1] + \
+                prepped.y_data['y_obs_trn_mean'][1]
     mask = ~(np.isnan(obs))
     assert np.allclose(processed[mask], obs[mask])
 

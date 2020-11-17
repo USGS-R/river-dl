b3ff99cc3eafdc64ada5f19a5caa753f95bff7ef
jsadler2 <jsadler@usgs.gov>
2020-11-16 21:37:44 -0600

    [#80] rm weights args in train_model, add lambda, support grad_adj

--- river_dl/train_model.py
+++ river_dl/train_model.py
@@ -19,16 +19,15 @@ parser.add_argument("-f", "--finetune_epochs", help='number of finetune'
                     'epochs', type=int)
 parser.add_argument("-q", "--flow-in-temp", help='whether or not to do flow\
                     in temp', action='store_true') 
-parser.add_argument("--pt_temp_wgt", help='weight for temp rmse in pretraining',
-                    type=float, default=0.5) 
-parser.add_argument("--ft_temp_wgt", help='weight for temp rmse in finetuning',
-                    type=float, default=0.5)
 parser.add_argument("--pt_learn_rate", help='learning rate for pretraining',
                     type=float, default=0.005)
 parser.add_argument("--ft_learn_rate", help='learning rate for finetuning',
                     type=float, default=0.01)
 parser.add_argument("--model", help="type of model to train",
                     choices=['lstm', 'rgcn'], default='rgcn')
+                    default='rgcn')
+parser.add_argument("--lamb", help="lambda for weighting aux gradient",
+                    default=1.0, type=float)
 
 
 args = parser.parse_args()
@@ -38,14 +37,12 @@ hidden_units = args.hidden_units
 out_dir = args.outdir
 pt_epochs = args.pretrain_epochs
 ft_epochs = args.finetune_epochs
-pt_temp_wgt = args.pt_temp_wgt
-ft_temp_wgt = args.ft_temp_wgt
 
 # -------- train ------
 model = train_model(in_data_file, pt_epochs, ft_epochs, hidden_units,
-                    out_dir=out_dir, flow_in_temp=flow_in_temp,
-                    finetune_temp_rmse_weight=ft_temp_wgt,
-                    pretrain_temp_rmse_weight=pt_temp_wgt,
+                    out_dir=out_dir, flow_in_temp=flow_in_temp, lamb=args.lamb,
                     seed=args.random_seed, learning_rate_ft=args.ft_learn_rate,
-                    learning_rate_pre=args.pt_learn_rate, model=args.model)
+                    learning_rate_pre=args.pt_learn_rate,
+                    model_type=args.model)
+
 

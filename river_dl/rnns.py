c582d3d2567daa75cc8a0bc90b3dce6ac5d01a85
jsadler2 <jsadler@usgs.gov>
2021-06-02 17:20:30 -0500

    Convert to MultitaskLSTM; update GRU classes

--- river_dl/rnns.py
+++ river_dl/rnns.py
@@ -37,61 +37,48 @@ class SingletaskLSTMModel(tf.keras.Model):
         return main_prediction
 
 
-class LSTMModel(tf.keras.Model):
+class MultitaskLSTMModel(tf.keras.Model):
     def __init__(
         self,
         hidden_size,
         gradient_correction=False,
-        tasks=1, 
         lambda_aux=1,
         recurrent_dropout=0,
         dropout=0,  
         grad_log_file=None,
-        return_state=False,
     ):
         """
         :param hidden_size: [int] the number of hidden units
         :param gradient_correction: [bool] 
-        :param tasks: [int] number of prediction tasks to perform - currently supports either 1 or 2 prediction tasks 
-        :param lambda_aux: [float] 
+        :param lambda_aux: [float]
         :param recurrent_dropout: [float] value between 0 and 1 for the probability of a reccurent element to be zero  
         :param dropout: [float] value between 0 and 1 for the probability of an input element to be zero  
         :param grad_log_file: [str] location of gradient log file 
-        :param return_state: [bool] return the hidden (h) and cell (c) states of LSTM 
         """
         super().__init__()
         self.gradient_correction = gradient_correction
         self.grad_log_file = grad_log_file
-        self.tasks = tasks 
         self.lambda_aux = lambda_aux
-        self.return_state = return_state 
         self.rnn_layer = layers.LSTM(
             hidden_size,
             return_sequences=True,
-            stateful=return_state,
-            return_state=return_state,
+            stateful=True,
+            return_state=True,
             name="rnn_shared",
             recurrent_dropout=recurrent_dropout,
             dropout=dropout
         )
         self.dense_main = layers.Dense(1, name="dense_main")
-        if self.tasks == 2: 
-            self.dense_aux = layers.Dense(1, name="dense_aux")
+        self.dense_aux = layers.Dense(1, name="dense_aux")
+        self.h = None
+        self.c = None
 
     @tf.function
     def call(self, inputs, **kwargs):
-        if self.return_state: 
-            x, h, c = self.rnn_layer(inputs)
-        else:
-            x = self.rnn_layer(inputs)
-            
-        if self.tasks == 1: 
-            main_prediction = self.dense_main(x) 
-            return main_prediction
-        else: 
-            main_prediction = self.dense_main(x)
-            aux_prediction = self.dense_aux(x)
-            return tf.concat([main_prediction, aux_prediction], axis=2)
+        x, self.h, self.c = self.rnn_layer(inputs)
+        main_prediction = self.dense_main(x)
+        aux_prediction = self.dense_aux(x)
+        return tf.concat([main_prediction, aux_prediction], axis=2)
 
     @tf.function
     def train_step(self, data):
@@ -104,46 +91,51 @@ class LSTMModel(tf.keras.Model):
             y_pred = self(x, training=True)  # forward pass
 
             loss_main = nnse_one_var_samplewise(y, y_pred, 0, self.tasks)
-            if self.tasks == 2:
-                loss_aux = nnse_one_var_samplewise(y, y_pred, 1, self.tasks)
+            loss_aux = nnse_one_var_samplewise(y, y_pred, 1, self.tasks)
 
         trainable_vars = self.trainable_variables
 
         main_out_vars = get_variables(trainable_vars, "dense_main")
-        if self.tasks == 2:
-            aux_out_vars = get_variables(trainable_vars, "dense_aux")
-            shared_vars = get_variables(trainable_vars, "rnn_shared")
+        aux_out_vars = get_variables(trainable_vars, "dense_aux")
+        shared_vars = get_variables(trainable_vars, "rnn_shared")
 
         # get gradients
         gradient_main_out = tape.gradient(loss_main, main_out_vars)
-        if self.tasks == 2:
-            gradient_aux_out = tape.gradient(loss_aux, aux_out_vars)
-            gradient_shared_main = tape.gradient(loss_main, shared_vars)
-            gradient_shared_aux = tape.gradient(loss_aux, shared_vars)
+        gradient_aux_out = tape.gradient(loss_aux, aux_out_vars)
+        gradient_shared_main = tape.gradient(loss_main, shared_vars)
+        gradient_shared_aux = tape.gradient(loss_aux, shared_vars)
 
-        if self.tasks == 2:
-            if self.gradient_correction:
-                # adjust auxiliary gradient
-                gradient_shared_aux = adjust_gradient_list(
-                    gradient_shared_main, gradient_shared_aux, self.grad_log_file
-                )
-        if self.tasks == 2:
-            combined_gradient = combine_gradients_list(
-                gradient_shared_main, gradient_shared_aux, lambda_aux=self.lambda_aux
+        if self.gradient_correction:
+            # adjust auxiliary gradient
+            gradient_shared_aux = adjust_gradient_list(
+                gradient_shared_main, gradient_shared_aux, self.grad_log_file
             )
+        combined_gradient = combine_gradients_list(
+            gradient_shared_main, gradient_shared_aux, lambda_aux=self.lambda_aux
+        )
 
         # apply gradients
         self.optimizer.apply_gradients(zip(gradient_main_out, main_out_vars))
-        if self.tasks == 2:
-            self.optimizer.apply_gradients(zip(gradient_aux_out, aux_out_vars))
-            self.optimizer.apply_gradients(zip(combined_gradient, shared_vars))
-            return {"loss_main": loss_main, "loss_aux": loss_aux}
-        else:
-            return {"loss_main": loss_main}
+        self.optimizer.apply_gradients(zip(gradient_aux_out, aux_out_vars))
+        self.optimizer.apply_gradients(zip(combined_gradient, shared_vars))
+        return {"loss_main": loss_main, "loss_aux": loss_aux}
 
 
+class SingletaskGRUModel(SingletaskLSTMModel):
+    def __init__(
+            self,
+            hidden_size,
+    ):
+        """
+        :param hidden_size: [int] the number of hidden units
+        """
+        super().__init__(hidden_size)
+        self.rnn_layer = layers.GRU(
+            hidden_size, return_sequences=True, name="rnn_shared"
+        )
 
-class GRUModel(LSTMModel):
+
+class MultitaskGRUModel(MultitaskLSTMModel):
     def __init__(
         self,
         hidden_size,

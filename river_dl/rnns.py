7c36a4f76604adda03be2afdbd13e4a623db0460
jsadler2 <jsadler@usgs.gov>
2021-06-02 17:16:41 -0500

    adding SingletaskLSTMModel

--- river_dl/rnns.py
+++ river_dl/rnns.py
@@ -4,6 +4,39 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from river_dl.loss_functions import nnse_masked_one_var, nnse_one_var_samplewise
 
+
+class SingletaskLSTMModel(tf.keras.Model):
+    def __init__(
+            self,
+            hidden_size,
+            recurrent_dropout=0,
+            dropout=0,
+    ):
+        """
+        :param hidden_size: [int] the number of hidden units
+        :param recurrent_dropout: [float] value between 0 and 1 for the probability of a reccurent element to be zero
+        :param dropout: [float] value between 0 and 1 for the probability of an input element to be zero
+        """
+        super().__init__()
+        self.rnn_layer = layers.LSTM(
+            hidden_size,
+            return_sequences=True,
+            stateful=True,
+            return_state=True,
+            recurrent_dropout=recurrent_dropout,
+            dropout=dropout
+        )
+        self.dense_main = layers.Dense(1, name="dense_main")
+        self.h = None
+        self.c = None
+
+    @tf.function
+    def call(self, inputs, **kwargs):
+        x, self.h, self.c = self.rnn_layer(inputs)
+        main_prediction = self.dense_main(x)
+        return main_prediction
+
+
 class LSTMModel(tf.keras.Model):
     def __init__(
         self,
@@ -71,43 +104,43 @@ class LSTMModel(tf.keras.Model):
             y_pred = self(x, training=True)  # forward pass
 
             loss_main = nnse_one_var_samplewise(y, y_pred, 0, self.tasks)
-            if self.tasks == 2: 
+            if self.tasks == 2:
                 loss_aux = nnse_one_var_samplewise(y, y_pred, 1, self.tasks)
 
         trainable_vars = self.trainable_variables
 
         main_out_vars = get_variables(trainable_vars, "dense_main")
-        if self.tasks == 2: 
+        if self.tasks == 2:
             aux_out_vars = get_variables(trainable_vars, "dense_aux")
             shared_vars = get_variables(trainable_vars, "rnn_shared")
 
         # get gradients
         gradient_main_out = tape.gradient(loss_main, main_out_vars)
-        if self.tasks == 2: 
+        if self.tasks == 2:
             gradient_aux_out = tape.gradient(loss_aux, aux_out_vars)
             gradient_shared_main = tape.gradient(loss_main, shared_vars)
             gradient_shared_aux = tape.gradient(loss_aux, shared_vars)
 
-        if self.tasks == 2: 
+        if self.tasks == 2:
             if self.gradient_correction:
                 # adjust auxiliary gradient
                 gradient_shared_aux = adjust_gradient_list(
                     gradient_shared_main, gradient_shared_aux, self.grad_log_file
                 )
-        if self.tasks == 2: 
+        if self.tasks == 2:
             combined_gradient = combine_gradients_list(
                 gradient_shared_main, gradient_shared_aux, lambda_aux=self.lambda_aux
             )
 
         # apply gradients
         self.optimizer.apply_gradients(zip(gradient_main_out, main_out_vars))
-        if self.tasks == 2: 
+        if self.tasks == 2:
             self.optimizer.apply_gradients(zip(gradient_aux_out, aux_out_vars))
             self.optimizer.apply_gradients(zip(combined_gradient, shared_vars))
             return {"loss_main": loss_main, "loss_aux": loss_aux}
-        else: 
-            return {"loss_main": loss_main} 
-        
+        else:
+            return {"loss_main": loss_main}
+
 
 
 class GRUModel(LSTMModel):

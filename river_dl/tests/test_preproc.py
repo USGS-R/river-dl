c438961d922ebb9e2f4e629a51c7ccef5be234e3
jsadler2 <jsadler@usgs.gov>
2022-02-17 16:40:35 -0500

    [#160] test val_sites, test_sites functionality

--- river_dl/tests/test_preproc.py
+++ river_dl/tests/test_preproc.py
@@ -2,7 +2,7 @@ import pytest
 from river_dl import preproc_utils
 
 # segments in test dataset
-segments = [2007, 2012]
+segs = [2012, 2007, 2014, 2037]
 # date range in test dataset
 min_date = "2003-09-15"
 max_date = "2006-10-15"
@@ -287,4 +287,150 @@ def test_prep_data_no_train():
                 x_vars=["seg_rain", "seg_tave_air"],
                 y_vars_finetune=["temp_c", "discharge_cms"],
             )
+
+
+def test_prep_data_val_test_sites():
+    prepped_data = preproc_utils.prep_all_data(
+            x_data_file="test_data/test_data",
+            y_data_file="test_data/obs_temp_flow",
+            pretrain_file="test_data/test_data",
+            train_start_date="2003-09-15",
+            train_end_date="2004-09-16",
+            val_start_date="2004-09-17",
+            val_end_date="2005-09-18",
+            val_sites=[2007],
+            test_sites=[2012],
+            spatial_idx_name="segs_test",
+            time_idx_name="times_test",
+            x_vars=["seg_rain", "seg_tave_air"],
+            y_vars_finetune=["temp_c", "discharge_cms"],
+            y_vars_pretrain=["seg_tave_water", "seg_outflow"],
+        )
+
+    assert "x_trn" in prepped_data.keys()
+    assert "x_val" in prepped_data.keys()
+    assert "x_tst" in prepped_data.keys()
+    assert "ids_trn" in prepped_data.keys()
+    assert "ids_val" in prepped_data.keys()
+    assert "ids_tst" in prepped_data.keys()
+    assert "times_trn" in prepped_data.keys()
+    assert "times_val" in prepped_data.keys()
+    assert "times_tst" in prepped_data.keys()
+    assert "y_pre_trn" in prepped_data.keys()
+    assert "y_pre_full" in prepped_data.keys()
+    assert "y_obs_trn" in prepped_data.keys()
+    assert "y_obs_val" in prepped_data.keys()
+    assert "y_obs_tst" in prepped_data.keys()
+
+    assert 2007 not in prepped_data["ids_trn"] 
+    assert 2012 not in prepped_data["ids_trn"] 
+    assert 2014 in prepped_data["ids_trn"] 
+    assert 2037 in prepped_data["ids_trn"] 
+
+    assert 2007 in prepped_data["ids_val"] 
+    assert 2014 in prepped_data["ids_val"] 
+    assert 2037 in prepped_data["ids_val"] 
+    assert 2012 not in prepped_data["ids_val"] 
+
+    assert prepped_data["ids_tst"] is None
+
+
+def test_prep_data_val_test_sites_test_dates():
+    prepped_data = preproc_utils.prep_all_data(
+            x_data_file="test_data/test_data",
+            y_data_file="test_data/obs_temp_flow",
+            pretrain_file="test_data/test_data",
+            train_start_date="2003-09-15",
+            train_end_date="2004-09-16",
+            val_start_date="2004-09-17",
+            val_end_date="2005-09-18",
+            test_start_date="2005-09-19",
+            test_end_date="2006-09-20",
+            val_sites=[2007],
+            test_sites=[2012],
+            spatial_idx_name="segs_test",
+            time_idx_name="times_test",
+            x_vars=["seg_rain", "seg_tave_air"],
+            y_vars_finetune=["temp_c", "discharge_cms"],
+            y_vars_pretrain=["seg_tave_water", "seg_outflow"],
+        )
+
+    assert "x_trn" in prepped_data.keys()
+    assert "x_val" in prepped_data.keys()
+    assert "x_tst" in prepped_data.keys()
+    assert "ids_trn" in prepped_data.keys()
+    assert "ids_val" in prepped_data.keys()
+    assert "ids_tst" in prepped_data.keys()
+    assert "times_trn" in prepped_data.keys()
+    assert "times_val" in prepped_data.keys()
+    assert "times_tst" in prepped_data.keys()
+    assert "y_pre_trn" in prepped_data.keys()
+    assert "y_pre_full" in prepped_data.keys()
+    assert "y_obs_trn" in prepped_data.keys()
+    assert "y_obs_val" in prepped_data.keys()
+    assert "y_obs_tst" in prepped_data.keys()
+
+    assert 2007 not in prepped_data["ids_trn"] 
+    assert 2012 not in prepped_data["ids_trn"] 
+    assert 2014 in prepped_data["ids_trn"] 
+    assert 2037 in prepped_data["ids_trn"] 
+
+    assert 2007 in prepped_data["ids_val"] 
+    assert 2014 in prepped_data["ids_val"] 
+    assert 2037 in prepped_data["ids_val"] 
+    assert 2012 not in prepped_data["ids_val"] 
+
+    assert 2012 in prepped_data["ids_tst"]
+    assert 2007 in prepped_data["ids_tst"] 
+    assert 2014 in prepped_data["ids_tst"] 
+    assert 2037 in prepped_data["ids_tst"] 
+
+
+def test_prep_data_no_val_tst_sites_test_dates():
+    prepped_data = preproc_utils.prep_all_data(
+            x_data_file="test_data/test_data",
+            y_data_file="test_data/obs_temp_flow",
+            pretrain_file="test_data/test_data",
+            train_start_date="2003-09-15",
+            train_end_date="2004-09-16",
+            val_start_date="2004-09-17",
+            val_end_date="2005-09-18",
+            test_start_date="2005-09-19",
+            test_end_date="2006-09-20",
+            spatial_idx_name="segs_test",
+            time_idx_name="times_test",
+            x_vars=["seg_rain", "seg_tave_air"],
+            y_vars_finetune=["temp_c", "discharge_cms"],
+            y_vars_pretrain=["seg_tave_water", "seg_outflow"],
+        )
+
+    assert "x_trn" in prepped_data.keys()
+    assert "x_val" in prepped_data.keys()
+    assert "x_tst" in prepped_data.keys()
+    assert "ids_trn" in prepped_data.keys()
+    assert "ids_val" in prepped_data.keys()
+    assert "ids_tst" in prepped_data.keys()
+    assert "times_trn" in prepped_data.keys()
+    assert "times_val" in prepped_data.keys()
+    assert "times_tst" in prepped_data.keys()
+    assert "y_pre_trn" in prepped_data.keys()
+    assert "y_pre_full" in prepped_data.keys()
+    assert "y_obs_trn" in prepped_data.keys()
+    assert "y_obs_val" in prepped_data.keys()
+    assert "y_obs_tst" in prepped_data.keys()
+
+    assert 2007 in prepped_data["ids_trn"] 
+    assert 2012 in prepped_data["ids_trn"] 
+    assert 2014 in prepped_data["ids_trn"] 
+    assert 2037 in prepped_data["ids_trn"] 
+
+    assert 2007 in prepped_data["ids_val"] 
+    assert 2014 in prepped_data["ids_val"] 
+    assert 2037 in prepped_data["ids_val"] 
+    assert 2012 in prepped_data["ids_val"] 
+
+    assert 2012 in prepped_data["ids_tst"]
+    assert 2007 in prepped_data["ids_tst"] 
+    assert 2014 in prepped_data["ids_tst"] 
+    assert 2037 in prepped_data["ids_tst"] 
 
